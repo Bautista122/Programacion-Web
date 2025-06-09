52d01052811cd7f1925dc950e0ec47af738c986b
Bautista <bautistamoyano1606@gmail.com>
2025-06-09 14:50:46 -0300

    segui con los ejercicios que me faltaron de java (termino lo que me quedo en casa)

--- html-css/actividad.js
+++ html-css/actividad.js
@@ -3,9 +3,31 @@ const contraseña = "1234";
 
 let usuarioInput = document.getElementById("valorUsuario").value
 let contraseñaInput = document.getElementById("valorContraseña").value
+const session = localStorage.getItem("session")
 
-if (usuarioInput === "Federico" && contraseñaInput === "1234") {
-    console.log("Usuario y contraseña correctos");
-} else { 
-    console.log("Usuario o contraseña incorrecto, vuelva a intentarlo");
+
+// const getSession = async ()=> {
+//
+//    if(session){
+//        const usuarios = await obtenerUsuarios()
+//        console.log(usuarios)
+//    }else{
+//        console.log("no hay session")
+//    }
+// }
+// getSession()
+
+const inicioSesion =async ()=>{
+
+    if (usuarioInput === "Federico" && contraseñaInput === "1234") {
+        console.log("Usuario y contraseña correctos");
+    
+    localStorage.setItem("session", true);
+    console.log(localStorage.getItem("session"));
+    const usuarios = await obtenerUsuarios();
+    
+    } else { 
+        console.log("Usuario o contraseña incorrecto, vuelva a intentarlo");
+    }
 }
+
