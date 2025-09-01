c889af1f42a2fdd4e7b79bebf5a8aed303126008
Bautista <bautistamoyano1606@gmail.com>
2025-09-01 11:01:00 -0300

    Termine los ejercicios

--- spread-operator/src/components/operator.jsx
+++ spread-operator/src/components/operator.jsx
@@ -1,34 +1,39 @@
 import { useState } from "react";
 
-
 export function Operator() {
- const [nums, setNums] = useState([1, 2, 3, 4, 5]);
- const [persona, setPersona] = useState({
-   name: "Federico",
-   alumno: "Thiago",
- });
+  const [nums, setNums] = useState([1, 2, 3, 4, 5]);
+  const [persona, setPersona] = useState({
+    name: "Federico",
+    alumno: "Thiago",
+  });
 
+  //arrow function
+  const addNums = () => {
+    setNums([...nums, nums.length + 1]);
+  };
 
- //arrow function
- const addNums = () => {
-   setNums([...nums, nums.length]);
- };
+  const updateName = () => {
+    setPersona({ ...persona, name: "Miguel", edad: 80 });
+    console.log(persona);
+  };
 
+  const productos = [
+    { nombre: "Camiseta", precio: 20 },
+    { nombre: "Pantalon", precio: 30 },
+  ];
 
- const updateName = () => {
-   setPersona({ ...persona, name: "Miguel", edad: 80 });
-   console.log(persona);
- };
-
-
- return (
-   <>
-     {nums.map((num, index) => (
-       <p key={index}>{num}</p>
-     ))}
-     <h3>Spread Operator</h3>
-     <button onClick={addNums}>agregar numeros</button>
-     <button onClick={updateName}>modificar persona</button>
-   </>
- );
+  const productosEnPromocion = productos.map((producto) => ({
+    ...producto,
+    descuento: true,
+  }));
+  return (
+    <>
+      {nums.map((num, index) => (
+        <p key={index}>{num}</p>
+      ))}
+      <h3>Spread Operator</h3>
+      <button onClick={addNums}>agregar numeros</button>
+      <button onClick={updateName}>modificar persona</button>
+    </>
+  );
 }
