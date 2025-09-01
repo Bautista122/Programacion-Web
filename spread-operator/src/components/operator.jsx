1de07c044ce1b093f4489a9f514c85c91223c7ec
Bautista <bautistamoyano1606@gmail.com>
2025-09-01 16:19:38 -0300

    Entregue los ejercicios y empezamos a trabajaron sobre nuestro proyecto

--- spread-operator/src/components/operator.jsx
+++ spread-operator/src/components/operator.jsx
@@ -7,14 +7,13 @@ export function Operator() {
     alumno: "Thiago",
   });
 
-  //arrow function
   const addNums = () => {
     setNums([...nums, nums.length + 1]);
   };
 
   const updateName = () => {
     setPersona({ ...persona, name: "Miguel", edad: 80 });
-    console.log(persona);
+    console.log("Persona actualizada:", persona);
   };
 
   const productos = [
@@ -26,14 +25,24 @@ export function Operator() {
     ...producto,
     descuento: true,
   }));
+
+  console.log("Productos en promoción:", productosEnPromocion);
+
   return (
     <>
       {nums.map((num, index) => (
         <p key={index}>{num}</p>
       ))}
+
       <h3>Spread Operator</h3>
-      <button onClick={addNums}>agregar numeros</button>
-      <button onClick={updateName}>modificar persona</button>
+      <button onClick={addNums}>Agregar números</button>
+
+      <h4>Productos en Promoción</h4>
+      {productosEnPromocion.map((p, i) => (
+        <p key={i}>
+          {p.nombre} - ${p.precio} {p.descuento ? "(Promoción)" : ""}
+        </p>
+      ))}
     </>
   );
 }
