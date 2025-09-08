f1e62f39bfe7186a73dfb8ff2eb4eff065224e4b
Bautista <bautistamoyano1606@gmail.com>
2025-09-08 10:57:03 -0300

    hicimos algunos endpoints basico para comenzar

--- endpoints-nodes/server.js
+++ endpoints-nodes/server.js
@@ -10,7 +10,34 @@ app.get("/", (req, res) => {
   res.send("Este es el primer endpoint hecho con express");
 });
 
+//endpoint con parametro
+app.get("/api/user/:id", (req, res) => {
+  //destructuracion
+  const { id } = req.params;
+  if (typeof id === "string") {
+    const idInt = parseInt(id); //parseInt  convierte de string en int
+    console.log(idInt);
+  }
+  res.json({ message: `Este es el id dado ${id}` });
+});
+
 //Iniciar server
 app.listen(port, () => {
   console.log(`El servidor corriendo en ${port}`);
+});
+
+//POST
+app.post("/api/user", (req, res) => {
+  const { name, email } = req.body;
+  res.json({ message: "Usuario creado", datos: { name, email } });
+});
+
+//query params
+app.get("/api/search", (req, res) => {
+  const { name, lastname } = req.query;
+  res.json({
+    firstName: name, //renombramos
+    lastname, //reutilizamos
+  });
+  //http://localhost:3000/api/search?name=Bautista&lastname=Moyano
 });
